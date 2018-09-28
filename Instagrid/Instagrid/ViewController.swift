b3504c15b2000d2a58fcfd8919a73ea92ca9dd3e
cgmuskat <c_garcia_muskat@hotmail.com>
2018-09-28 12:36:40 +0200

    Instagrid project created with xcode file + first steps in View folder with first icon img

---
+++ Instagrid/Instagrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Carlos Garcia-Muskat on 28/09/2018.
+//  Copyright © 2018 Carlos Garcia-Muskat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

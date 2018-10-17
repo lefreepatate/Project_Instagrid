48bea17ac981adfe19a507f7db8627fda9bfc6f3
cgmuskat <c_garcia_muskat@hotmail.com>
2018-10-17 17:55:46 +0200

    Landscape and portraits views ok. Working on menu buttons to change the stackview photo distribution

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -7,49 +7,66 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBOutlet weak var gridArea: UIView!
-    @IBOutlet weak var grid1: UIStackView!
-    @IBOutlet weak var grid3: UIStackView!
-    @IBOutlet weak var grid2: UIStackView!
-    @IBOutlet var gridButtons: [UIStackView]!
-    
-    /*override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }*/
-    enum gridStyle {
-        case grid1, grid2, grid3
-    }
-    var style:gridStyle = .grid1 {
+    var icon = UIImageView() {
         didSet {
-            setView(style)
+            icon.image = #imageLiteral(resourceName: "plus_icon")
         }
     }
-    private func setView(_ style: gridStyle){
-        switch style {
-        case .grid1:
-            grid2.isHidden = true
-            grid3.isHidden = true
-        case .grid2:
-            grid1.isHidden = true
-            grid3.isHidden = true
-        case .grid3:
-            grid1.isHidden = true
-            grid2.isHidden = true
+    @IBOutlet weak var PhotoGrid: UIStackView!
+    @IBOutlet weak var topGrid:UIStackView!
+    @IBOutlet weak var bottomGrid: UIStackView!
+    var button:UIButton! {
+        didSet {
+            icon.image = #imageLiteral(resourceName: "plus_icon")
+            button.backgroundColor = #colorLiteral(red: 0.9410838485, green: 0.9412414432, blue: 0.9410631061, alpha: 1)
+            icon.isHidden = false
+            button.isHidden = false
         }
     }
-    @IBAction func didTapGrid1() {
-        getNewGrid()
+    
+    @IBAction func button1(_ sender: UIButton) {
+        getGrid1()
     }
-    private func getNewGrid(){
-        
+    @IBAction func button2(_ sender: UIButton) {
+        getGrid2()
     }
+    @IBAction func button3(_ sender: UIButton) {
+        getGrid3()
+    }
+    @objc func reset(){
+        let grid = [UIStackView]()
+        for views in grid{
+            topGrid.removeArrangedSubview(views)
+            bottomGrid.removeArrangedSubview(views)
+        }
+    }
+     @objc func getGrid1() {
+        reset()
+        topGrid.addArrangedSubview(button)
+        bottomGrid.addArrangedSubview(button)
+        bottomGrid.addArrangedSubview(button)
+    }
+    @objc func getGrid2() {
+        reset()
+        topGrid.addArrangedSubview(button)
+        topGrid.addArrangedSubview(button)
+        bottomGrid.addArrangedSubview(button)
+    }
+    @objc func getGrid3() {
+        reset()
+        topGrid.addArrangedSubview(button)
+        topGrid.addArrangedSubview(button)
+        bottomGrid.addArrangedSubview(button)
+        bottomGrid.addArrangedSubview(button)
+    }
+
 }
 

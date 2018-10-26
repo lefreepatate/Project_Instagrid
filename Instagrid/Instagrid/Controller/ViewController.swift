f9905599b2aa2b1970f1c814690da0b0c1495e0b
cgmuskat <c_garcia_muskat@hotmail.com>
2018-10-26 12:25:51 +0200

    New access to library

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import Foundation
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         /*let name = Notification.Name(rawValue: "Grid")
          NotificationCenter.default.addObserver(self, selector: #selector(getGrid1), name: name, object: nil)*/
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,33 +19,55 @@ class ViewController: UIViewController {
     @IBOutlet var photoGrid: [UIStackView]!
     @IBOutlet weak var topGrid:UIStackView!
     @IBOutlet weak var bottomGrid: UIStackView!
+    @IBOutlet weak var tapButton: UILabel!
+    @IBAction func UpLoadButton(_ sender: UIButton) {
+        _ = makeButton()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    let imagePicker = UIImagePickerController()
+    
     @IBAction func button1(_ sender: UIButton) {
         getGrid1()
     }
+    
     @IBAction func button2(_ sender: UIButton) {
         getGrid2()
     }
+    
     @IBAction func button3(_ sender: UIButton) {
         getGrid3()
     }
     
+    var touchCount = 0
+    
+    lazy var button = makeButton()
+    
     var identifyButton = 0
+    
     func getUniqueButton(button:UIButton) -> Int {
         identifyButton += 1
-        return identifyButton
+        if button.state == .highlighted {
+            touchCount += 1
+        }
+        tapButton.text = "Number of buttons: \(identifyButton)\nButton touched: \(touchCount)"
+        return identifyButton + touchCount
     }
-
+    
     func makeButton() -> UIButton {
         let button = UIButton()
-         _ = getUniqueButton(button: button)
         button.backgroundColor = #colorLiteral(red: 0.9410838485, green: 0.9412414432, blue: 0.9410631061, alpha: 1)
         button.setImage(#imageLiteral(resourceName: "plus_icon"), for: UIControl.State.normal)
         button.setImage(#imageLiteral(resourceName: "plus_icon_over"), for: UIControl.State.highlighted)
+        _ = getUniqueButton(button: button)
         return button
     }
     
     @objc func reset(){
         identifyButton = 0
+        touchCount = 0
         let subViews = topGrid.arrangedSubviews + bottomGrid.arrangedSubviews
         for view in subViews {
             view.removeFromSuperview()
@@ -58,6 +81,7 @@ class ViewController: UIViewController {
         bottomGrid.addArrangedSubview(view())
         bottomGrid.addArrangedSubview(view())
     }
+    
     @objc func getGrid2() {
         reset()
         let view = makeButton
